deabd85f710f96dccafa2917aed5832b3f9a16b6
Christopher Horrell <christopher@joyent.com>
2013-12-04 20:52:35 +0000

    enable URL checking in motd and product for base images

--- spec/base-common/motd_product_spec.rb
+++ spec/base-common/motd_product_spec.rb
@@ -6,11 +6,11 @@ describe file('/etc/product') do
   it { should be_file }
   it { should contain "Name: Joyent Instance" }
   it { should contain "Image: #{attr[:name]} #{attr[:base_version]}"  }
-	#it { should contain "Documentation: #{attr[:doc_url]}" }
+	it { should contain "Documentation: #{attr[:doc_url]}" }
 end
 
 describe file('/etc/motd') do
 	it { should be_file }
 	it { should contain "Instance (#{attr[:name]} #{attr[:base_version]})" }
-	#it { should contain "#{attr[:doc_url]}" }
+	it { should contain "#{attr[:doc_url]}" }
 end
