87e04ca7c102c258c7e0a110ed14dc0a8f874948
Christopher Horrell <christopher@joyent.com>
2014-01-08 19:39:30 +0000

    Fix hbase test, makes sure to kill off java when done so test is repeatable

--- spec/hadoop/hbase_spec.rb
+++ spec/hadoop/hbase_spec.rb
@@ -5,7 +5,7 @@ describe command('start-hbase.sh') do
   it { should return_exit_status 0 }
 end
 
-# Make sure hbase stops without error as well
-describe command('stop-hbase.sh') do
+# Make sure Java process is killed off when done
+describe command('pkill -9 java') do
   it { should return_exit_status 0 }
 end
