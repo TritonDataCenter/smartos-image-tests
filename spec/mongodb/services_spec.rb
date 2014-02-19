5fed0e7eb65736d2c3e56036a80934cccb2f92cc
Christopher Horrell <christopher@joyent.com>
2014-02-19 02:37:30 +0000

    test to ensure we can connec to mongodb

--- spec/mongodb/services_spec.rb
+++ spec/mongodb/services_spec.rb
@@ -7,3 +7,8 @@ end
 describe service('mongodb') do
   it { should be_running }
 end
+
+# Make sure we can log into mongodb
+describe command('mongostat -uadmin -p$(mdata-get mongodb_pw) -n1') do
+	it { should return_exit_status 0 }
+end
