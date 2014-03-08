5b8cc757595422c0d4d877b8275901a9a91930ea
Christopher Horrell <christopher@joyent.com>
2014-03-08 00:28:53 +0000

    Fixed test for login. IMAGE-455

--- spec/postgresql/logins_spec.rb
+++ spec/postgresql/logins_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 # Test login to posgres works
-describe command('su - postgres -c "PGPASSWORD=$(mdata-get pgsql_pw)" psql -U postgres -c "select version();"') do
+describe command('PGPASSWORD="$(mdata-get pgsql_pw)" psql -U postgres -c "select version();"') do
   it { should return_exit_status 0 }
 end
