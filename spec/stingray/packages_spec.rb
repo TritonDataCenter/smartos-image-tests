b758f8051f59456a9a005a72374a73e87440edc2
Christopher Horrell <christopher@joyent.com>
2014-04-01 13:12:16 +0000

    Check for manta

--- spec/stingray/packages_spec.rb
+++ spec/stingray/packages_spec.rb
@@ -15,3 +15,13 @@ describe package('openjdk7') do
   it { should be_installed }
 end
 
+# Check for Manta
+describe file('/opt/local/lib/node_modules/manta/') do
+	it { should be_directory }
+end
+
+# Check for a respresentative symlink
+describe file('/opt/local/bin/mls') do
+	it { should be_linked_to '/opt/local/lib/node_modules/manta/bin/mls'
+end
+
