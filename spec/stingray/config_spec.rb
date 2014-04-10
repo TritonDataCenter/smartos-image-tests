8310f05e680b38a25bd97de13692848249f09a55
Christopher Horrell <christopher@joyent.com>
2014-04-10 18:13:55 +0000

    Just test for /opt/local/stingray/zxtm-9.5/.backup/ dir

--- spec/stingray/config_spec.rb
+++ spec/stingray/config_spec.rb
@@ -5,24 +5,16 @@ require 'spec_helper'
 describe file('/opt/local/stingray/zxtm/conf/settings.cfg') do
   it { should be_file }
 	# IMAGE-411 Java Extensions enabled
-	it { should contain "java!enabled Yes" }
+	it { should contain 'java!enabled    Yes' }
 	# Added setting "webcache!max_file_size 25MB" to zxtm configuration
-	it { should contain "webcache!max_file_size 25MB" }
+	it { should contain('25MB').after(/^webcache!max_file_size/) }
 end
 
 
-# IMAGE-430 missing directories on stingray images interferes with importing \
+# IMAGE-430 missing directory on stingray images interferes with importing \
 # backups via the webUI
 
 describe file('/opt/local/stingray/zxtm-9.5/.backup/') do
   it { should be_directory }
-end
-
-describe file('/opt/local/stingray/zxtm-9.5/.backup/conf/') do
-  it { should be_directory }
-end
-
-describe file('/opt/local/stingray/zxtm-9.5/.backup/log/') do
-  it { should be_linked_to '/opt/local/stingray/zxtm-9.5/log/' }
 end
 
