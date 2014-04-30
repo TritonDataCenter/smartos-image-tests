3f184f49d3dc02728f15317712e8ac8f435cd417
Derek Crudgington <derek@joyent.com>
2014-04-30 20:31:01 +0000

    Added tests for mongodb 13.4.2. IMAGE-482

--- spec/mongodb/configs_spec.rb
+++ spec/mongodb/configs_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
-describe file('/opt/local/etc/mongodb.conf') do
-  it { should be_file }
-  it { should contain "dbpath = /var/mongodb" }
-  it { should contain "bind_ip = 127.0.0.1" }
-  it { should contain "port = 27017" }
-  it { should contain "pidfilepath = /var/mongodb/mongodb.pid" }
-  it { should contain "logpath = /var/log/mongodb/mongodb.log" }
+if attr[:base_version].delete('.').to_i <= 1331
+  describe file('/opt/local/etc/mongodb.conf') do
+    it { should be_file }
+    it { should contain "dbpath = /var/mongodb" }
+    it { should contain "bind_ip = 127.0.0.1" }
+    it { should contain "port = 27017" }
+    it { should contain "pidfilepath = /var/mongodb/mongodb.pid" }
+    it { should contain "logpath = /var/log/mongodb/mongodb.log" }
+  end
+end
+
+# MongoDB 2.6.0 and onward changes configuration file. DATASET-1006
+if attr[:base_version].delete('.').to_i >= 1342
+  describe file('/opt/local/etc/mongod.conf') do
+    it { should be_file }
+    it { should contain "dbpath=/var/mongodb" }
+    it { should contain "bind_ip = 127.0.0.1" }
+    it { should contain "port = 27017" }
+    it { should contain "pidfilepath = /var/mongodb/mongod.pid" }
+    it { should contain "logpath=/var/log/mongodb/mongod.log" }
+    it { should contain "nohttpinterface = true" }
+  end
 end
--- spec/mongodb/packages_spec.rb
+++ spec/mongodb/packages_spec.rb
@@ -18,6 +18,13 @@ describe package('mms-agent') do
 end
 
 ## Version specific checks for mongodb
+# Since 13.4.2. See IMAGE-482
+if attr[:base_version].delete('.').to_i == 1342
+	describe package('mongodb-2.6.0') do
+  	it { should be_installed }
+	end
+end
+
 # Since 13.3.1. See IMAGE-429
 if attr[:base_version].delete('.').to_i == 1330 || attr[:base_version].delete('.').to_i == 1331
 	describe package('mongodb-2.4.9') do
