c477557780f9b2b550ce1cb968c06a23ecd74c28
Derek Crudgington <derek@joyent.com>
2014-05-14 14:53:17 +0000

    Added tests for postgresql-14.1.0.  IMAGE-508

--- spec/postgresql/configs_spec.rb
+++ spec/postgresql/configs_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
-# Make sure postgresql is listening on the private ip
-
-privateIp = `/usr/sbin/mdata-get sdc:nics.1.ip`
-
-describe file('/var/pgsql/data/postgresql.conf') do
-   it { should be_file }
-   it { should contain "listen_addresses = '" + privateIp + "'" }
+describe command ('PRIVATE_IP=$(mdata-get sdc:nics.1.ip) && grep -q "listen_addresses = \'${PRIVATE_IP}\'" /var/pgsql/data/postgresql.conf') do
+  it { should return_exit_status 0 }
 end
 
+describe command ('PRIVATE_IP=$(mdata-get sdc:nics.1.ip) && egrep -q "host\s+all\s+all\s+${PRIVATE_IP}/32\s+password" /var/pgsql/data/pg_hba.conf') do
+  it { should return_exit_status 0 }
+end
--- spec/postgresql/services_spec.rb
+++ spec/postgresql/services_spec.rb
@@ -3,4 +3,5 @@ require 'spec_helper'
 # Default services for image
 describe service('postgresql') do
   it { should be_running }
+  it { should be_enabled }
 end
