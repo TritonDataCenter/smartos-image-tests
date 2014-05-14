4dac8fd461b04ce253bec9c975662ac0b907fb56
Derek Crudgington <derek@joyent.com>
2014-05-14 19:12:10 +0000

    Added tests for nodejs-14.1.0.  IMAGE-466

--- spec/nodejs/services_spec.rb
+++ spec/nodejs/services_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
-if attr[:base_version].delete('.').to_i < 1340
 describe service('mongodb') do
+  it { should be_running }
   it { should be_enabled }
 end
+
+# Make sure we can log into mongodb
+describe command('mongostat -uadmin -p$(mdata-get mongodb_pw) -n1') do
+        it { should return_exit_status 0 }
 end
 
+# Make sure quickbackup-mongodb works
+describe command('quickbackup-mongodb backup') do
+        it { should return_exit_status 0 }
+end
