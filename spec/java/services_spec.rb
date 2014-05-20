7448caf31ab90da179f7179087deb1841ca6c65e
Derek Crudgington <derek@joyent.com>
2014-05-20 14:34:40 +0000

    Added tests for java-14.1.0.  IMAGE-510

--- spec/java/services_spec.rb
+++ spec/java/services_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe service('apache') do
   it { should be_enabled }
+  it { should be_running }
 end
 
 describe service('tomcat') do
+  it { should be_enabled }
   it { should be_running }
 end
