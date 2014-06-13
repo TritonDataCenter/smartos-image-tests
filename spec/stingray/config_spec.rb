0d4fa41bf345da4c78acb4a2126ced34c056fe9a
Derek Crudgington <derek@joyent.com>
2014-06-13 14:00:03 +0000

    Added tests for stingray 14.1.0.  IMAGE-528

--- spec/stingray/config_spec.rb
+++ spec/stingray/config_spec.rb
@@ -16,15 +16,17 @@ end
 
 if attr[:name] == "stm-developer"
 	# stm-developer has Stingray 9.6
-  describe file('/opt/local/stingray/zxtm-9.6/.backup/') do
+  describe file('/opt/local/stingray/zxtm/.backup/') do
     it { should be_directory }
   end
 else
 	# All other 13.4.1 versions have 9.5
-  describe file('/opt/local/stingray/zxtm-9.5/.backup/') do
+  describe file('/opt/local/stingray/zxtm/.backup/') do
     it { should be_directory }
   end
-  
 end
 
-
+# Make sure the license key name matches the one in the file
+describe command ('VAR1=$(basename $(ls -d /opt/local/stingray/zxtm/conf/licensekeys/*)); VAR2=$(grep "Serial Number" /opt/local/stingray/zxtm/conf/licensekeys/* | awk \'{ print $5 }\'); [[ "${VAR1}" = "${VAR2}" ]] && echo "YES" || echo "NO"') do
+  it { should return_stdout "YES" }
+end
