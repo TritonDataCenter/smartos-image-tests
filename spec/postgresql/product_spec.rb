78fe3cf3ea42843dfa6695e96eda067a275bf0d3
Christopher Horrell <christopher@joyent.com>
2014-08-21 15:06:37 +0000

    Replaced attr with property to work with newer Serverspec

--- spec/postgresql/product_spec.rb
+++ spec/postgresql/product_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe file('/etc/product') do
   it { should be_file }
-  it { should contain "Base Image: #{attr[:base_name]} #{attr[:base_version]}"  }
+  it { should contain "Base Image: #{property[:base_name]} #{property[:base_version]}"  }
 end
 
