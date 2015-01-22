5c808f925c56241723f823784304ceac07f90339
Derek Crudgington <derek@joyent.com>
2015-01-22 18:23:14 +0000

    Added base_name base_version check

--- spec/base-common/motd_product_spec.rb
+++ spec/base-common/motd_product_spec.rb
@@ -6,6 +6,7 @@ describe file('/etc/product') do
   it { should be_file }
   it { should contain "Name: Joyent Instance" }
   it { should contain "Image: #{property[:name]} #{property[:version]}"  }
+  it { should contain "Base Image: #{property[:base_name]} #{property[:base_version]}"  }
   it { should contain "Documentation: #{property[:doc_url]}" }
 end
 
