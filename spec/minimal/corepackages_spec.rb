70efff4692284844931aec5001e2ec534bc1a98c
Christopher Horrell <christopher@joyent.com>
2015-04-20 10:12:24 -0400

    changepass no preinstalled in minimal images

--- spec/minimal/corepackages_spec.rb
+++ spec/minimal/corepackages_spec.rb
@@ -7,14 +7,18 @@ end
 
 ## Common packages
 
+describe package('changepass') do
+  it { should be_installed }
+end
+
+describe package('mozilla-rootcerts') do
+  it { should be_installed }
+end
+
 describe package('smtools') do
   it { should be_installed }
 end
 
 describe package('zoneinit') do
-  it { should be_installed }
-end
-
-describe package('mozilla-rootcerts') do
   it { should be_installed }
 end
