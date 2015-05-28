c9656a0984954b8be675de6496d0599ce44344ad
Christopher Horrell <christopher@joyent.com>
2015-05-28 09:16:06 -0400

    Add tests for directory permissions (/etc and /var)
    See IMAGE-743 and IMAGE-744

---
+++ spec/base-common/directory_permissions_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+# Test to ensure files and directories under /etc are not owned by the
+# '1000' user (the owner should be root)
+# See IMAGE-743 and IMAGE-744
+#
+# The find command always returns 0 unless there was an error. To
+# Get around that we pipe to grep which returns 0 for matches and 1
+# for no matches. The expected value should be 1 -- no directories owned
+# by the 1000 user.
+describe command('find /etc -user 1000 -ls | grep 1000') do
+  its(:exit_status) { should eq 1 }
+end
+
+
+# Test to ensure files directories under /var are not owned by the
+# '1000' user (the owner should be root)
+# See IMAGE-743 and IMAGE-744
+describe command('find /var -user 1000 -ls | grep 1000') do
+  its(:exit_status) { should eq 1 }
+end
