82c5a1bff4cf8fdf18f3e842380bcc76255a5dd9
Christopher Horrell <christopher@joyent.com>
2015-10-29 10:22:40 -0400

    add test for sm-create-dbuser and sm-remove-dbuser

--- spec/percona/sm-tools_spec.rb
+++ spec/percona/sm-tools_spec.rb
@@ -40,6 +40,33 @@ describe command('sm-create-db') do
   its(:exit_status) { should eq 2 }
 end
 
+# sm-create-dbuser
+
+describe command('sm-create-dbuser mysql testdbuser CavrybcysUng9Oli testdb') do
+  its(:stdout) { should match /\* Created MySQL user testdbuser pass CavrybcysUng9Oli with access to testdb from anywhere./ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('sm-list-dbusers mysql') do
+  its(:stdout) { should match /testdbuser/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('sm-create-dbuser') do
+  its(:exit_status) { should eq 2 }
+end
+
+# sm-remove-dbuser
+
+describe command('sm-remove-dbuser mysql testdbuser') do
+  its(:stdout) { should match /\* Removed MySQL user testdbuser./ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('sm-remove-dbuser') do
+  its(:exit_status) { should eq 2 }
+end
+
 # sm-remove-db
 
 describe command('sm-remove-db mysql testdb') do
@@ -50,3 +77,4 @@ end
 describe command('sm-remove-db') do
   its(:exit_status) { should eq 2 }
 end
+
