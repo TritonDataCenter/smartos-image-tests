4dd42dce104972cc024348f5cce086a4e245fe44
Christopher Horrell <christopher@joyent.com>
2015-10-29 09:55:04 -0400

    Add sm-create-db test

--- spec/percona/sm-tools_spec.rb
+++ spec/percona/sm-tools_spec.rb
@@ -25,3 +25,20 @@ describe command('sm-list-dbusers') do
   its(:exit_status) { should eq 2 }
   its(:stderr) { should match /Usage: \/opt\/local\/bin\/sm-list-dbusers [options] TYPE/ }
 end
+
+# sm-create-db
+
+describe command('sm-create-db mysql testdb') do
+  its(:stdout) { should match /\* Created MySQL database testdb./ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('sm-list-dbs mysql') do
+  its(:stdout) { should match /testdb/ }
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('sm-create-db') do
+  its(:exit_status) { should eq 2 }
+  its(:stderr) { should match /Usage: \/opt\/local\/bin\/sm-list-dbusers [options] TYPE DATABASE/ }
+end
