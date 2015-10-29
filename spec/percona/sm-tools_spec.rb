f96670be4f255dd9ed0064d72c2170f433f7b82e
Christopher Horrell <christopher@joyent.com>
2015-10-29 09:44:45 -0400

    Add test for sm-tools: sm-list-dbs

---
+++ spec/percona/sm-tools_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+# sm-list-dbs
+
+describe command('sm-list-dbs mysql') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('sm-list-dbs') do
+  its(:exit_status) { should eq 2 }
+  its(:stderr) { should match /Usage: \/opt\/local\/bin\/sm-list-dbs [options] TYPE/ }
+end
+
+describe command('sm-list-dbs mysql') do
+  its(:stdout) { should match /information_schema/ }
+  its(:stdout) { should match /mysql/ }
+  its(:stdout) { should match /mysql/ }
+  its(:stdout) { should match /performance_schema/ }
+end
