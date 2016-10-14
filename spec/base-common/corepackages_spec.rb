a53c050bd9f832b1926ddf1d3990139f4cb21a92
Christopher Horrell <christopher@joyent.com>
2016-10-14 10:52:09 -0400

    comment tweak

--- spec/base-common/corepackages_spec.rb
+++ spec/base-common/corepackages_spec.rb
@@ -25,7 +25,7 @@ end
 
 ## Since 13.3.0
 # See IMAGE-340, IMAGE-404
-# rsyslog pre-installed and running out of the box (in place of legacy syslog); has guardtime support built-in
+# rsyslog pre-installed and running out of the box (in place of legacy syslog)
 if property[:base_version].delete('.').to_i >= 1330
   describe package('rsyslog') do
   it { should be_installed }
